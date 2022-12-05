a15be43bbcc36629be66411cd49504f3f6f7b5ca
sucolova <lukas.melcher@gmail.com>
2022-12-05 11:08:35 +0100

    useEffect implemented

--- src/features/comments/comments.js
+++ src/features/comments/comments.js
@@ -9,29 +9,34 @@ export const Comments = (props) => {
   const dispatch = useDispatch();
   const comments = useSelector(selectComments);
   const permalink = props.permalink;
-  const [commentsToRender, setCommentsToRender] = useState("");
-  let areCommentsFetched = comments[permalink];
+  const [commentsToRender, setCommentsToRender] = useState();
+  let areCommentsFetched = comments[permalink]; // true if store.comments has a property permalink, it only has this property, if the comments for the post, it is in were fetched
+  let commentsList = commentsToRender ? <ul>{commentsToRender}</ul> : null; 
+  // console.log('comments were rendered')
 
   const handleClick = () => {
     dispatch(getComments(permalink));
   }
-  useEffect(() => {
-    if (areCommentsFetched) {
-      setCommentsToRender( comments[permalink].map((comment) => {
-        return (
-          <Comment body={comment.comment} author={comment.author} key={uuidv4()} /> 
-        )
-      })
-      )
 
+  useEffect(() => { // this way comments are only updated if they are fetched
+    console.log('useEffect in Comments');
+    if (areCommentsFetched) { // still need that condition, because useEffect always runs in the first render
+      setCommentsToRender( comments[permalink].map(
+        (comment) => {
+          return (
+            <Comment body={comment.comment} author={comment.author} key={uuidv4()} /> 
+          )
+        }
+      ));
     }
-  }, [areCommentsFetched]) // ich will im deparray eigentlich nur areCommentsFetched
+    // eslint-disable-next-line
+  }, [areCommentsFetched]) // the other dependencies are not neccessary, and would cause the effect to run to often.
 
 
   return (
     <div>
       <button onClick={handleClick} > getComments </button>
-      <ul>{commentsToRender}</ul>
+      {commentsList}
     </div>
   )
 }
