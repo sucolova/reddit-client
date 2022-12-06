0441492509a0509cd86066a2fd4055170976732e
sucolova <lukas.melcher@gmail.com>
2022-12-06 16:57:40 +0100

    core functionality working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Posts } from './features/posts/posts';
 import { SubReddits } from './features/subReddits/subReddits';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header>
         <h2>Reddit client</h2>
+        <SubReddits />
       </header>
-      <SubReddits />
-      <Posts/>
+      <main>
+        <Posts/>
+      </main>
     </div>
   );
 }
