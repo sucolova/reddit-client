30c4f848389285e6a8dd893768cc2f1c4f694a6c
sucolova <lukas.melcher@gmail.com>
2022-12-07 09:52:58 +0100

    fontawesome add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Posts } from './features/posts/posts';
 import { SubReddits } from './features/subReddits/subReddits';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import {faRedditAlien} from '@fortawesome/free-brands-svg-icons';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header>
+        <FontAwesomeIcon icon={faRedditAlien} />
+        <FontAwesomeIcon icon={faCoffee} />
         <h2>Reddit client</h2>
         <SubReddits />
       </header>
